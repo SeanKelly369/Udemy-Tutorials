404f23ffb54ea4abe20294caba932702b938a3d4
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-12-03 10:15:26 +0000

    Add files via upload

--- Angular_app/src/app/app.module.ts
+++ Angular_app/src/app/app.module.ts
@@ -1,26 +1,35 @@
-import { DataService } from './service/data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { UserComponent} from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeService } from './recipes/recipe.service';
+import { DataStorageService } from './shared/data-storage.service';
+import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth-guard.service';
+import { RecipesModule } from './recipes/recipes.modules';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    UsersComponent,
-    NavbarComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpModule,
+    AppRoutingModule,
+    RecipesModule,
+    SharedModule,
+    ShoppingListModule,
+    AuthModule
   ],
-  providers: [DataService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
